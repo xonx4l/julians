4b7c545932852e9cb9e6e884d1f83ac32869b8a4
xonx4l <sk797119@gmail.com>
2024-03-18 23:32:44 +0530

    update set

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use image::{ImageBuffer, Rgb};
+use nalgebra::{complex, Normed};
+
+
 fn main() {
-    println!("Hello, world!");
-}
+       let width = 800;
+       let height = 600;
+
+       let scale_x = 3.0 / width as f64;
+       let scale_y = 3.0 / height as f64;
+
+       let mut img = ImageBuffer::new(width, height);
+
+       for (x,y, pixel) in img.enumerate_pixels_mut() {
+              *pixel = Rgb ([100, 100, 100]);
+       }
+
+       img.save("julia.png");
+
+    }
